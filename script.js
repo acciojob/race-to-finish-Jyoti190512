09d2f2de76e8cde45d1bfb32966daff969cb3064
Jyoti190512 <134208281+Jyoti190512@users.noreply.github.com>
2023-08-13 23:37:58 +0100

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,24 @@
-
-
-// Do not change the code above this
-// add your promises to the array `promises`
 window.promises = [];
-
-// Function to create a promise that resolves after a random time
-function createPromise() {
-    return new Promise(resolve => {
-        const randomTime = Math.floor(Math.random() * 5000) + 1000; // Random time between 1 and 5 seconds
-        setTimeout(() => {
-            resolve(randomTime);
-        }, randomTime);
-    });
-}
-
-// Populate the promises array with 5 promises
+ 
+// Create an array of five promises with random delays between 1 and 5 seconds
 for (let i = 0; i < 5; i++) {
-    window.promises.push(createPromise());
+  const delay = Math.floor(Math.random() * 5) + 1;
+  const promise = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`Promise ${i + 1} resolved after ${delay} seconds`);
+    }, delay * 1000);
+  });
+  promises.push(promise);
 }
-
-// Use Promise.any() to wait for the first promise to resolve
-Promise.any(window.promises)
-    .then(result => {
-        // Print the result to the output div
-        const outputDiv = document.getElementById("output");
-        outputDiv.textContent = `First promise resolved with time: ${result} milliseconds`;
-    })
-    .catch(error => {
-        console.error("All promises rejected:", error);
-    });
-
+ 
+// Wait for the first promise to resolve using Promise.any()
+Promise.any(promises)
+  .then((result) => {
+    // Print the result to the output div
+    const outputDiv = document.getElementById('output');
+    outputDiv.textContent = result;
+  })
+  .catch((error) => {
+    // Handle errors if no promise resolves
+    console.log('All promises rejected:', error);
+  });
